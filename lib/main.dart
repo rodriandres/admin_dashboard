0483173823382a1c95fc86f1a411873e3016a52f
Rodev <rodriandres998@gmail.com>
2024-04-06 20:55:05 -0300

    implement mock token in localstorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:admin_dashboard/providers/auth_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
+import 'package:admin_dashboard/services/local_storage.dart';
 import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await LocalStorage.configurePrefs();
   Flurorouter.configuredRoutes();
   runApp(AppState());
 }
@@ -33,7 +35,10 @@ class MyApp extends StatelessWidget {
       title: 'Admin Dashboard',
       initialRoute: Flurorouter.rootRoute,
       onGenerateRoute: Flurorouter.router.generator,
+      // navigatorKey: ,
       builder: (_, child) {
+        // print(LocalStorage.prefs.getString('token'));
+
         return AuthLayout(child: child!);
       },
       theme: ThemeData.light().copyWith(
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 
+import 'package:admin_dashboard/services/local_storage.dart';
 import 'package:flutter/material.dart';
 
 class AuthProvider extends ChangeNotifier {
@@ -10,16 +11,16 @@ class AuthProvider extends ChangeNotifier {
     // HTTP REQUEST
     _token = 'kajsndjkasjdknasjkd1232njajsd.sdjn2';
     print('Storage JWT: $_token');
+    LocalStorage.prefs.setString('token', _token!);
 
     // TODO Navigate to dashboard
 
     notifyListeners();
   }
 
-    register( String name, String email, String password) {
+  register( String name, String email, String password) {
     // HTTP REQUEST
-    _token = 'kajsndjkasjdknasjkd1232njajsd.sdjn2';
-    print('Storage JWT: $_token');
+    print('Register');
 
     // TODO Navigate to dashboard
 
